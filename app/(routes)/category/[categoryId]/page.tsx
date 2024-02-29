2051902c05ce2be03336ded8d9aae334851e9a0b
jon-jc <jonathancho.jc@gmail.com>
2024-02-29 13:59:54 -0800

    Add filter functionality and product card components
    feat: Category pages

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,10 @@ import getCategory from "@/actions/get-category";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 
+import Filter from "./components/filter";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
+
 export const revalidate = 0;
 
 interface CategoryPageProps {
@@ -35,6 +39,23 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div className="bg-white">
       <Container>
         <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* Add here Filters for mobile*/}
+            <div className="hidden lg:block">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
