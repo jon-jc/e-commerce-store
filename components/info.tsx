e6164a8ce2136bdc5fb9c6c32ac5458162b3e602
jon-jc <jonathancho.jc@gmail.com>
2024-02-29 13:25:54 -0800

    Fix bug causing color and price not to be fetched correctly.

--- components/info.tsx
+++ components/info.tsx
@@ -25,7 +25,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
           <div
-            className="h-6 w-6 rounded-full border border-gray-500"
+            className="h-6 w-6 rounded-full border border-gray-600"
             style={{ backgroundColor: data?.color?.value }}
           />
         </div>
