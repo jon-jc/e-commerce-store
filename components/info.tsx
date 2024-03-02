e52a0f7c7b367f51aec96cdaeaf8824147a8e345
jon-jc <jonathancho.jc@gmail.com>
2024-03-02 13:52:17 -0800

    Fix button disabled state and update cart functionality

--- components/info.tsx
+++ components/info.tsx
@@ -1,26 +1,36 @@
-import { Product } from "@/types";
+"use client";
+
+import { ShoppingCart } from "lucide-react";
+
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/Button";
-import { ShoppingCart } from "lucide-react";
+import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold text-slate-700">{data.name}</h1>
+      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
-        <p className="text-2xl text-gray-500">
-          <Currency value={data.price} />
+        <p className="text-2xl text-gray-900">
+          <Currency value={data?.price} />
         </p>
       </div>
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>{data?.size?.name}</div>
+          <div>{data?.size?.value}</div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
@@ -31,9 +41,9 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
-          Add to Cart
-          <ShoppingCart />
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Add To Cart
+          <ShoppingCart size={20} />
         </Button>
       </div>
     </div>
